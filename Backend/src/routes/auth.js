5625dd6730a7214bf5289dd5bd1ea774c69a7cbd
mendezleoa <leonardoamm1100@gmail.com>
2024-02-15 14:19:18 -0400

    Validaciones y login con jwt

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -1,13 +1,45 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+const Joi = require('@hapi/joi');
+
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+const bcrypt = require('bcrypt');
+
 router.post('/register', async (req, res) => {
+
+    const { error } = schemaRegister.validate(req.body)
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message })
+    }
+
+    const isEmailExist = await User.findOne({ email: req.body.email });
+    if (isEmailExist) {
+        return res.status(400).json(
+            { error: 'El Email está registrado' }
+        )
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
 
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password
     });
+
     try {
         const savedUser = await user.save();
         res.json({
@@ -16,8 +48,33 @@ router.post('/register', async (req, res) => {
         })
     }
     catch (error) {
-        res.status(400).json({error})
+        res.status(400).json({ error })
     }
 })
+
+router.post('/login', async (req, res) => {
+
+    const { error } = schemaLogin.validate(req.body)
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message })
+    }
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json({ error: 'Email/Contraseña no válida' });
+
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).json({ error: 'Email/Contraseña no válida' })
+
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id
+    }, process.env.TOKEN_SECRET)
+    
+    res.header('auth-token', token).json({
+        error: null,
+        data: {token}
+    })
+});
 
 module.exports = router;
